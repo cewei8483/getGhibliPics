54563aa517a048a26638d8c9fe4bf512819a8ece
7nwei <65698922+cewei8483@users.noreply.github.com>
2020-09-23 14:35:24 +0800

    fix the place of wget bug

--- getGhibliPics.bat
+++ getGhibliPics.bat
@@ -11,14 +11,33 @@ set animeList=marnie,kaguyahime,ged,kazetachinu,kokurikozaka,karigurashi,ponyo,c
 
 ::example pic URL: http://www.ghibli.jp/gallery/chihiro001.jpg
 
+for %%X in (wget.exe) do (set FOUND=%%~$PATH:X)
+IF EXIST wget.exe (
+	set state=1
+) ELSE if defined FOUND (
+	set state=2
+) ELSE (
+	echo Please put wget.exe in your current directory or in PATH.
+	pause
+	EXIT
+)
+::echo %state%
+
 for %%i in (%animeList%) do (
 	echo %%i
 	mkdir %%i
 	cd %%i
 	for /L %%j in (1, 1, %count%) do (
 		set formattedValue=000000%%j
-		::@echo "http://www.ghibli.jp/gallery/%%i!formattedValue:~-3!.jpg"
-		wget -nc "http://www.ghibli.jp/gallery/%%i!formattedValue:~-3!.jpg"
+		IF %state%==1 (
+			%cd%/wget.exe -nc "http://www.ghibli.jp/gallery/%%i!formattedValue:~-3!.jpg
+		) ELSE (
+			IF %state%==2 (
+				wget -nc "http://www.ghibli.jp/gallery/%%i!formattedValue:~-3!.jpg
+			)
+		)
 	)
 	cd ..
 )
+
+pause
